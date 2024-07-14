4e75cb23deb79b1b082a8fd12ed052c563901526
likhon29 <mdabdurrouf.likhon@gmail.com>
2024-07-14 17:25:03 +0600

    error fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { process } from "node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/my-portfolio",
+  base: "/my-portfolio",
 });
